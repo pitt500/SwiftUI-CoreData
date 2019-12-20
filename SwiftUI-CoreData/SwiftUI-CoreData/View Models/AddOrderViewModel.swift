e1350725ac1817ed4f604eeafeb228781519814b
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-19 17:57:13 -0700

    Create View and ViewModel to add order

---
+++ SwiftUI-CoreData/SwiftUI-CoreData/View Models/AddOrderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  AddOrderViewModel.swift
+//  SwiftUI-CoreData
+//
+//  Created by projas on 12/19/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class AddOrderViewModel {
+  var name = ""
+  var type = ""
+}
---
+++ SwiftUI-CoreData/SwiftUI-CoreData/Views/AddOrderView.swift
@@ -0,0 +1,44 @@
+//
+//  AddOrderView.swift
+//  SwiftUI-CoreData
+//
+//  Created by projas on 12/19/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddOrderView: View {
+  
+  @State var addOrderVM = AddOrderViewModel()
+  
+  var body: some View {
+    NavigationView {
+      Group {
+        VStack {
+          TextField("Enter name", text: self.$addOrderVM.name)
+          Picker(selection: self.$addOrderVM.type, label: Text("")) {
+            Text("Cappuccino").tag("cap")
+            Text("Regular").tag("reg")
+            Text("Expresso").tag("ex")
+          }.pickerStyle(SegmentedPickerStyle())
+          
+          Button("Place Order") {
+            
+          }.padding(8)
+            .foregroundColor(Color.white)
+            .background(Color.green)
+            .cornerRadius(10)
+        }
+      }.padding()
+      
+        .navigationBarTitle("Add Order", displayMode: .inline)
+    }
+  }
+}
+
+struct AddOrderView_Previews: PreviewProvider {
+  static var previews: some View {
+    AddOrderView()
+  }
+}
